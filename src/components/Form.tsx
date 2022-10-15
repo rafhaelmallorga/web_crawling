6131f3d0707afd0ba7671b91bf72becae515f562
Rafhael Mallorga <rafhaelmallorga@gmail.com>
2022-10-15 01:44:48 -0300

    fix :hammer: fixed mobile

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,6 +5,7 @@ import { useForm } from "react-hook-form"
 import { yupResolver } from "@hookform/resolvers/yup"
 import { NewInspectionInterface } from '../interfaces'
 
+
 const Form = () => {
     const { isDark, setIsDark, createNewInspection } = useWebCrawl()
     const [isLoading, setIsLoading] = useState<boolean>(false)
@@ -33,14 +34,14 @@ const Form = () => {
     }
 
     return (
-        <div className={`w-full max-w-[1000px] h-[180px] my-4 ${isDark ? 'bg-backFormDark' : 'bg-backForm'} shadow transition ease-in duration-500`}>
+        <div className={`w-full max-w-[1000px] h-[200px] ss:h-[180px] my-4 ${isDark ? 'bg-backFormDark' : 'bg-backForm'} shadow transition ease-in duration-500`}>
             <div className={`px-10 py-5 text-[22px] font-semibold ${isDark ? 'text-titleDark' : 'text-title'} transition ease-in duration-500`}>
                 <h3>Cadastre sua solicitação de inspeção</h3>
             </div>
             <form onSubmit={handleSubmit(handleNewInspection)} className={`px-10 w-full flex items-end`}>
                 <div className={`flex flex-col`}>
                     <label htmlFor="keyword" className={`text-[14px] font-semibold ${isDark ? 'text-labelDark' : 'text-label transition ease-in duration-500'}`}>Palavra chave:</label>
-                    <input type="text" {...register("keyword")} className={`h-[40px] rounded-sm mt-2 pl-4 outline-none font-normal ${isDark ? 'bg-backDark text-white' : 'bg-white text-title'} transition ease-in duration-500`} placeholder={'Ex: Security'}/>
+                    <input type="text" {...register("keyword")} className={`w-full h-[40px] rounded-sm mt-2 pl-4 outline-none font-normal ${isDark ? 'bg-backDark text-white' : 'bg-white text-title'} transition ease-in duration-500`} placeholder={'Ex: Security'}/>
                 </div>
                 <button type='submit' className={`bg-logo h-[40px] ml-8 px-4 rounded-sm text-white font-semibold hover:brightness-90`}>Cadastrar</button>
             </form>
--- src/components/InspectionItemList.tsx
+++ src/components/InspectionItemList.tsx
@@ -15,20 +15,25 @@ const InspectionItemList = ({ inspection }: IInspection) => {
 
 
     return (
-        <li key={inspection.id} className={`w-full h-[50px] px-10 flex justify-between items-center ${isDark ? 'bg-backDark text-labelDark border-borderDark' : 'bg-back text-label border-border'} border-b-[1px] ${isDark ? 'hover:bg-hoverDark' : 'hover:bg-hover'} transition ease-in duration-500`}>
-            <div className='w-[20%] flex justify-start items-center font-normal'>
+        <li key={inspection.id} className={`w-full h-[50px] text-[14px] ss:text-[16px] px-10 flex justify-between items-center ${isDark ? 'bg-backDark text-labelDark border-borderDark' : 'bg-back text-label border-border'} border-b-[1px] ${isDark ? 'hover:bg-hoverDark' : 'hover:bg-hover'} transition ease-in duration-500`}>
+            <div className='w-[30%] ss:w-[20%] flex justify-start items-center font-normal'>
                 <GrStatusCriticalSmall className={`mr-2 ${inspection.status === 'active' ? 'text-yellow-300' : 'text-green-500'}`}/>
                 <span>Status: <span className='font-semibold'>{inspection.status.toUpperCase()}</span></span>
             </div>
-            <span className='w-[30%] font-normal'>ID: <span className='font-semibold'>{inspection.id}</span></span>
+            <span className='w-[30%] ss:w-[20%] font-normal'>ID: <span className='font-semibold'>{inspection.id}</span></span>
             <button onClick={() => {
                 setModalIsOpen(!modalIsOpen)
                 setHandleUrls(inspection.urls)
-            }} className='flex justify-center items-center font-semibold bg-logo px-2 text-white rounded-sm'><FiPlusCircle className='mr-1' />urls</button>
-            <div className='w-[180px] flex justify-between items-center'>
-                <button onClick={() => retrieveInspectionById(inspection.id)} className={`flex justify-center items-center text-[14px] text-logo  ${inspection.status === 'done' && 'opacity-25 cursor-not-allowed'}`}>
+            }} className='flex justify-center items-center font-semibold bg-logo px-2 text-white rounded-sm'>
+                <FiPlusCircle className='ss:mr-1' />
+                <span className='hidden ss:block'>urls</span>
+            </button>
+            <div className='ss:w-[180px] flex justify-between items-center'>
+                <button onClick={() => retrieveInspectionById(inspection.id)} className={`flex mr-4 justify-center items-center text-[14px] text-logo  ${inspection.status === 'done' && 'opacity-25 cursor-not-allowed'}`}>
                     <BiLoaderCircle className='mr-1 text-[18px]' />
-                    Atualizar
+                    <span className='hidden ss:block'>
+                        Atualizar
+                    </span>
                 </button>
                 <button onClick={() => deleteInspectionById(inspection.id)}>
                     <RiDeleteBinFill color='red'/>
